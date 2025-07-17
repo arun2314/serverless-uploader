a883c8b7710f1a2668e9f1add3ba19fdc31bd055
arun2314 <arunkumarkdnl483@gmail.com>
2025-07-17 19:09:09 +0000

    Added Jenkins deployment test print

--- lambda_function.py
+++ lambda_function.py
@@ -11,6 +11,7 @@ table_name = 'FileMetadata'        # We will create this table in Step 3
 
 def lambda_handler(event, context):
     try:
+        print("✅ Jenkins auto-deployment test")
         # Read file from API body (base64 encoded)
         file_content = base64.b64decode(event['body'])
 
